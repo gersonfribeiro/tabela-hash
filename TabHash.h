47fd2329f9da4419e68ac386576999a84781aee4
Dantton Schettini <161088767+d1ntton@users.noreply.github.com>
2024-11-26 20:00:02 -0300

    Create TabHash.h

---
+++ TabHash.h
@@ -0,0 +1,21 @@
+#ifndef TABHASH_H
+#define TABHASH
+
+
+#include <string>
+using namespace std;
+
+// Declaração de funções
+void HASH_COSTRUTOR();
+
+void HASH_REMOVER(string chave);
+
+void AlmentarTabela();
+
+int funcaoHash(string chave);
+
+void HASH_INSERIR(string chave);
+
+void HASH_EXIBIR();
+
+#endif //TABHASH_H
